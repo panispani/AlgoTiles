fd4dac1ddf20412bf7c04ffcde74b72bcdf3e7dc
panpan2 <p.panayiotou2@gmail.com>
2016-06-25 00:19:34 +0300

    [image processing] tile splitting works, test methods added as well

--- ImageProcessing.java
+++ ImageProcessing.java
@@ -1,4 +1,7 @@
 import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.*;
 
 public class ImageProcessing {
 
@@ -12,16 +15,16 @@ public class ImageProcessing {
     /*
      * set every time, specific to each capture
      */
-    private int imageWidth;
-    private int imageHeight;
-    private int tileWidth;
-    private int tileHeight;
+    private static int imageWidth;
+    private static int imageHeight;
+    private static int tileWidth;
+    private static int tileHeight;
 
     /*
      *  Return array of commands from input image
      *  Assume fullscreen, resinfg could be done later
      */
-    public int[] run(BufferedImage image) {
+    public static int[] run(BufferedImage image) {
         BufferedImage[] tiles = splitImage(image);
         return null;
     }
@@ -30,7 +33,7 @@ public class ImageProcessing {
      * Splits image into tiles
      * tiles are TILES_ROW * TILES_COL in total
      */
-    private BufferedImage[] splitImage(BufferedImage image) {
+    private static BufferedImage[] splitImage(BufferedImage image) {
         // set every time
         imageWidth = image.getWidth();
         imageHeight = image.getHeight();
@@ -41,9 +44,41 @@ public class ImageProcessing {
         //break image into tiles
         for(int i = 0; i < TILES_ROW; i++) {
             for(int j = 0; j < TILES_COL; j++) {
-                tiles[i * TILES_COL + j] = new BufferedImage(tileWidth, tileHeight, image.getType());
+                //reserve memory
+                tiles[i * TILES_COL + j] =
+                    new BufferedImage(tileWidth, tileHeight, image.getType());
+                //draw tiles
+                Graphics2D renderer = tiles[i * TILES_COL + j].createGraphics();
+                renderer.drawImage(image, 0, 0, tileWidth, tileHeight,
+                        tileWidth * j, tileHeight * i,
+                        tileWidth * j + tileWidth, tileHeight * i + tileHeight, null);
+                renderer.dispose();
             }
         }
+        test(tiles);
         return tiles;
+    }
+
+    /*
+     * Functions for testing purposes
+     */
+    private static void test(BufferedImage[] tiles) {
+        for(int i = 0; i < tiles.length; i++) {
+            try {
+                ImageIO.write(tiles[i], "jpg", new File("tile" + i + ".jpg"));
+            } catch (Exception e) {
+                System.out.println("Error in writting tile " + i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        File input = new File("board.jpg");
+        try {
+            BufferedImage image = ImageIO.read(input);
+            run(image);
+        } catch (Exception e) {
+            System.out.println("Error reading input image");
+        }
     }
 }
