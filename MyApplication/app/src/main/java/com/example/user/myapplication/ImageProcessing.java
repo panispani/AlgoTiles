4761ed7d4e12a0e02a3f9fcbafcff7496e838469
panpan2 <p.panayiotou2@gmail.com>
2016-06-25 12:37:05 +0300

    [project join] work on joining project

---
+++ MyApplication/app/src/main/java/com/example/user/myapplication/ImageProcessing.java
@@ -0,0 +1,196 @@
+package com.example.user.myapplication;
+
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+import java.io.*;
+import java.awt.*;
+
+public class ImageProcessing {
+
+    /*
+     * Tile information, global and specific to
+     * the playboard
+     */
+    public static int TILES_ROW = 10;
+    public static int TILES_COL = 5;
+    public static int NUM_COLORS = 17; // 7 + 10 numbers
+    // enum would be better
+    /*
+    public static int WHITE = 0x1;      // number 
+    public static int BLACK = 0x2;     // end
+    public static int RED = 0x3;       // for
+    public static int GREEN = 0x4;     // move
+    public static int BLUE = 0x5;      // left
+    public static int YELLOW = 0x6;    //right
+    public static int PINK = 0x7;      // if
+    public static int LIGHTBLUE = 0x8; // else
+    public static int GREY = 0x9;      // beads
+*/
+    public static enum ourColors{
+        Black,
+        White,
+        Gray,
+        Yellow,
+        Green,
+        Cyan,
+        Blue,
+        Magenta,
+        Red,
+        Empty
+    }
+    /*
+     * set every time, specific to each capture
+     */
+    private static int imageWidth;
+    private static int imageHeight;
+    private static int tileWidth;
+    private static int tileHeight;
+
+    /*
+     *  Return array of commands from input image
+     *  Assume fullscreen, resinfg could be done later
+     */
+    public static ourColors[] run(BufferedImage image) {
+        BufferedImage[] tiles = splitImage(image);
+        ourColors[] commands = new ourColors[tiles.length];
+        for(int i = 0; i < tiles.length; i++) {
+            System.out.print(i+"\t");
+            commands[i] = getCommand(tiles[i]);
+        }
+        return commands;
+    }
+
+    /*
+     * Splits image into tiles
+     * tiles are TILES_ROW * TILES_COL in total
+     */
+    private static BufferedImage[] splitImage(BufferedImage image) {
+        // set every time
+        imageWidth = image.getWidth();
+        imageHeight = image.getHeight();
+        tileWidth = imageWidth / TILES_COL;
+        tileHeight = imageHeight / TILES_ROW;
+
+        BufferedImage[] tiles = new BufferedImage[TILES_ROW * TILES_COL];
+        //break image into tiles
+        for(int i = 0; i < TILES_ROW; i++) {
+            for(int j = 0; j < TILES_COL; j++) {
+                //reserve memory
+                tiles[i * TILES_COL + j] =
+                    new BufferedImage(tileWidth, tileHeight, image.getType());
+                //draw tiles
+                Graphics2D renderer = tiles[i * TILES_COL + j].createGraphics();
+                renderer.drawImage(image, 0, 0, tileWidth, tileHeight,
+                        tileWidth * j, tileHeight * i,
+                        tileWidth * j + tileWidth, tileHeight * i + tileHeight, null);
+                renderer.dispose();
+            }
+        }
+        test(tiles);
+        return tiles;
+    }
+
+    /*
+     * Returns command number corresponding to input image tile
+     */
+    private static ourColors getCommand(BufferedImage tile) {
+        if(tile == null) {
+            System.out.println("Input image is null");
+        }
+        int[] frequency = new int[NUM_COLORS];
+        int color;
+        ourColors mostFrequent = ourColors.Empty;
+        int maxFrequency = 0;
+        for(int x = 0; x < tileWidth; x++) {
+            for(int y = 0; y < tileHeight; y++) {
+                color = tile.getRGB(x, y);
+                ourColors classifyResult= classify(color);
+                //System.out.println(tile.hashCode()+""+classifyResult);
+                short colorReturned = (short) classifyResult.ordinal();
+                frequency[colorReturned]++;
+                if(frequency[colorReturned] > maxFrequency) {
+                    mostFrequent = classify(color);
+                    maxFrequency = frequency[colorReturned];
+                } 
+            }
+        }
+        if(mostFrequent == ourColors.White || mostFrequent == ourColors.Gray) {
+            //TODO return countBeads(tile);
+        }
+        System.out.println(mostFrequent);
+        return mostFrequent;
+    }
+
+    /*
+     * Counts beads in input image using
+     * Hough Circle Trasform
+     */ 
+    public static int countBeads(BufferedImage image) {
+        return 0;
+    }
+
+    /*
+     * Gets input hex color and returns category of it
+     * Blue, Green, Red etc
+     */
+    private static int getSimpleColor(int hex) {
+        return 0;
+        //return range(hex2Rgb(new String(hex)));
+    } 
+
+    /*
+     * Functions for testing purposes
+     */
+    private static void test(BufferedImage[] tiles) {
+        for(int i = 0; i < tiles.length; i++) {
+            try {
+                ImageIO.write(tiles[i], "jpg", new File("tile" + i + ".jpg"));
+            } catch (Exception e) {
+                System.out.println("Error in writting tile " + i);
+            }
+        }
+    }
+
+    public static Color hex2Rgb(String colorStr) {
+        return new Color(
+                Integer.valueOf( colorStr.substring( 1, 3 ), 16 ),
+                Integer.valueOf( colorStr.substring( 3, 5 ), 16 ),
+                Integer.valueOf( colorStr.substring( 5, 7 ), 16 ) );
+    }
+    
+    public static ourColors classify(int color)
+    {
+        float[] hsbvals = new float[3];
+
+        Color.RGBtoHSB((color&0xFF0000)>>16, (color&0x00FF00)>>8, (color&0x0000FF)>>0, hsbvals);
+        //System.out.println(((color&0xFF0000)>>16)+"-"+((color&0x00FF00)>>8)+"-"+(color&0x0000FF));
+        float hue = hsbvals[0]*360;
+        float sat = hsbvals[1];
+        float lgt = hsbvals[2];     
+
+       // System.out.println(hue+"-"+lgt+"-"+sat);
+        if (sat < 0.25) return ourColors.Gray;
+        if (lgt < 0.1)  return ourColors.Black;
+        if (lgt > 0.9)  return ourColors.White;
+
+        if (hue < 30)   return ourColors.Red;
+        if (hue < 90)   return ourColors.Yellow;
+        if (hue < 150)  return ourColors.Green;
+        if (hue < 210)  return ourColors.Cyan;
+        if (hue < 270)  return ourColors.Blue;
+        if (hue < 330)  return ourColors.Magenta;
+        return ourColors.Red;
+    }
+    public static void main(String[] args) {
+        File input = new File("sample.jpg");
+        try {
+            BufferedImage image = ImageIO.read(input);
+            run(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error reading input image");
+        }
+    }
+}
--- MyApplication/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -3,25 +3,15 @@ package com.example.user.myapplication;
 import android.content.pm.ActivityInfo;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.Point;
 import android.os.Bundle;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.os.Handler;
 import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import bsh.Interpreter;
-
-import java.lang.reflect.Array;
-import java.security.Timestamp;
-import java.util.Calendar;
 import java.util.Random;
 
 public class MainActivity extends Activity {
@@ -55,14 +45,13 @@ public class MainActivity extends Activity {
         });
     }
 
-    int dir=0;
     @Override
     protected void onStart(){
         super.onStart();
     }
     @Override
     protected void onResume(){
-        super.onResume();;
+        super.onResume();
 
     }
 
@@ -70,18 +59,15 @@ public class MainActivity extends Activity {
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
+            int[] commands = ImageProcessing.run(photo);
+            Simulate.simulate(grid, commands);
         }
     }
     int width = 10;
     int height = 10;
     RelativeLayout r1;
     ImageView temp;
-    int bombC=0;
-    int grassC=1;
-    int rockC=2;
-    int playerC =3;
-    int flagC =4;
-    int Array[][];
+    int grid[][];
     Bitmap bomb;
     Bitmap grass;
     Bitmap rock;
@@ -93,23 +79,21 @@ public class MainActivity extends Activity {
     ImageView[][] images;
 
     public void start(){
-        // Array
-        images=new ImageView[height][width];
-        Array = new int[height][width];
+        // grid
+        grid = new int[height][width];
         Random r = new Random();
         for (int x=0; x < height; x++){
             for (int y = 0; y < width; y++){
 
                 int i1 = (r.nextInt(3));
-                Array [x][y] = i1;
+                grid [x][y] = i1;
             };
         };
-        Array[0][0]=playerC;
-        Array[height-1][width-1]=flagC;
+        grid[0][0] = PLAYERUP;
+        grid[height-1][width-1] = FLAG;
         r1 = (RelativeLayout) findViewById(R.id.rl);
         r1.setBackground( getResources().getDrawable(R.drawable.grass));
         Display d = getWindowManager().getDefaultDisplay();
-
 
         int size = d.getWidth();
 
@@ -138,55 +122,29 @@ public class MainActivity extends Activity {
         flag = BitmapFactory.decodeResource(this.getResources(),R.drawable.flag);
         flag = Bitmap.createScaledBitmap(flag,size/height,size/width,false);
 
-
-
-
-
-        for (int x=0; x < height; x++){
+        for (int x = 0; x < height; x++){
             for (int y = 0; y < width; y++){
                 temp = new ImageView(this);
 
                 temp.setX(x*size/(float)height);
                 temp.setY(y*size/(float)width);
 
-                if (Array [x][y] == bombC){
-
-
+                if (grid [x][y] == BOMB){
                     temp.setImageBitmap(bomb);
                 }
-                if (Array [x][y] == rockC){
-
-
+                if (grid [x][y] == ROCK){
                     temp.setImageBitmap(rock);
                 }
-
-                if (Array [x][y] == playerC){
-
-                    switch(dir){
-                        case 0:
-                            temp.setImageBitmap(player0);
-                            break;
-                        case 1:
-                            temp.setImageBitmap(player1);
-                            break;
-                        case 2:
-                            temp.setImageBitmap(player2);
-                            break;
-                        case 3:
-                            temp.setImageBitmap(player3);
-                            break;
-
-
-                    }
+                // default player is looking up
+                if (grid [x][y] == PLAYER){
+                    temp.setImageBitmap(player0);
                 }
 
-                if (Array [x][y] == flagC){
-
-
+                if (grid [x][y] == FLAG){
                     temp.setImageBitmap(flag);
                 }
 
-                if (Array [x][y] == grassC){
+                if (grid [x][y] == GRASS){
                     temp.setImageBitmap(grass);
                 }
 
@@ -197,247 +155,5 @@ public class MainActivity extends Activity {
                 r1.addView(temp);
             }
         }
-
-
-
-    }
-
-
-    int playerX = 0;
-    int playerY = 0;
-
-    public boolean isGrass(int height,int width){
-        if(Array[height][width] == grassC){
-            return true;
-        }
-        else
-            return false;
-    }
-    public boolean isRock(int height,int width){
-        if(Array[height][width] == rockC){
-            return true;
-        }
-        else
-            return false;
-    }
-    public boolean isBomb(int height,int width){
-        return Array[height][width] == bombC;
-    }
-
-
-    int ForC = 0;
-    int IfC =1;
-    int EndC=2;
-    int ElseC=3;
-    int RightC =4;
-    int LeftC =5;
-    int MoveC=6;
-    int NumC=7;
-    void left(){
-        dir--;
-        if(dir<0){
-            dir=3;
-        }
-    }
-    void right(){
-        dir++;
-        if(dir>3){
-            dir=0;
-        }
-    }
-    int[] ar = {ForC,5+NumC,MoveC,EndC,RightC,ForC,5+NumC,MoveC,EndC};
-    int lastIndex=8;
-
-    int fun(int s){
-
-        if(s>lastIndex){
-            return -1;
-        }
-        if(ar[s]==EndC){
-            return s;
-        }
-
-        else if(ar[s]==ForC){
-            int k=ar[s+1]-NumC;
-            int e=s;
-            for(int i=0;i<k;i++){
-                e = fun (s+2);
-            }
-
-            if(fun(e+1)==-1){
-                return -1;
-            }
-
-        }
-        else if(ar[s]==IfC){
-            if(check(ar[s+1]-NumC)==0){
-                int l=fun(s+2);
-                return fun(l+1);
-            }
-            else{
-                while(ar[s]!=EndC&&ar[s]!=ElseC){
-                    s++;
-                }
-                if(ar[s]==EndC){
-                    return s;
-                }
-                else{
-                    return(s+1);
-                }
-            }
-        }
-        else if (ar[s]==MoveC){
-            move();
-        }
-        else if(ar[s]==LeftC){
-            left();
-        }
-        else if(ar[s]==RightC){
-            right();
-        }
-
-        if(s<lastIndex&&s!=-1){
-            return fun(s+1);
-        }
-        else {
-            return -1;
-        }
-    }
-    public int check(int num){
-        switch (dir){
-            case 0:
-                return validMoveRight(1,num);
-
-            case 1:
-                return validMoveDown(1,num);
-
-            case 2:
-                return validMoveLeft(1,num);
-
-            case 3:
-                return validMoveUp(1,num);
-
-        }
-      return 0;
-    }
-    public void move(){
-        if(check(1)!=0){
-            return;
-        }
-        Array[playerY][playerX]=grassC;
-        images[playerX][playerY].setImageBitmap(grass);
-        switch (dir){
-            case 0:
-               playerX++;
-                break;
-            case 1:
-               playerY++;
-                break;
-            case 2:
-                playerX--;
-                break;
-            case 3:
-             playerY--;
-                break;
-        }
-
-        updateGame();
-
-    }
-
-    public void updateGame(){
-        Array[playerY][playerX]=playerC;
-        switch (dir){
-            case 0:
-                images[playerX][playerY].setImageBitmap(player0);
-                break;
-            case 1:
-                images[playerX][playerY].setImageBitmap(player1);
-                break;
-            case 2:
-                images[playerX][playerY].setImageBitmap(player2);
-                break;
-            case 3:
-                images[playerX][playerY].setImageBitmap(player3);
-                break;
-        }
-
-    }
-
-    public int validMoveRight(int z, int move){
-        if(playerX+z>=width){
-            return 3;
-        }
-        if(isGrass(playerY, playerX + z)){
-            if (z==move){return 0;}
-            else
-                z++;
-            return validMoveRight(z, move);
-        }
-        if (isRock(playerY, playerX+z)){
-            return 1;
-        }
-        if (isBomb(playerY, playerX+z)){
-            return 2;
-        }
-        return -1;
-
-    }
-    public int validMoveLeft(int z, int move){
-        if(playerX-z<0){
-            return 3;
-        }
-        if(isGrass(playerY, playerX - z)){
-            if (z==move){return 0;}
-            else
-                z--;
-            return validMoveLeft(z, move);
-        }
-        if (isRock(playerY, playerX- z)){
-           return 1;
-        }
-        if (isBomb(playerY, playerX- z)){
-            return 2;
-        }
-        return -1;
-
-    }
-    public int validMoveUp(int z, int move){
-        if(playerY-z<0){
-            return 3;
-        }
-        if(isGrass(playerY - z, playerX)){
-            if (z==move){return 0;}
-            else
-                z--;
-            return validMoveUp(z, move);
-        }
-        if (isRock(playerY- z, playerX)){
-            return 1;
-        }
-        if (isBomb(playerY- z, playerX)){
-            return 2;
-        }
-        return -1;
-
-    }
-    public int validMoveDown(int z, int move){
-        if(playerY+z>=height){
-            return 3;
-        }
-        if(isGrass(playerY + z, playerX)){
-            if (z==move){return 0;}
-            else
-                z++;
-            return validMoveUp(z, move);
-        }
-        if (isRock(playerY+ z, playerX)){
-            return 1;
-        }
-        if (isBomb(playerY+ z, playerX)){
-            return 2;
-        }
-        return -1;
-
     }
 }
